9a6c434b5227c8f6ad58611c149bb509508406d8
EzraJRogers <applebo@applebos-Air.MG8702>
2024-06-23 23:25:09 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# cms
+
+# CMS
+
+## Description
+Create, Read, and Edit everything within a company so you can manage your employees bettter.
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Run commands npm i && node index.js
+
+## Usage
+To use just follow the prompted questions and use the tooltips to navigate the app.
+
+## License
+undefined
+
+## Contributing
+Ezra J Rogers
+
+## Tests
+No test instructions
+
+## Questions
+For additional questions, contact [undefined](https://github.com/undefined) or email [undefined](mailto:undefined).
--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,22 @@ USE employee_db;
 CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL
-
 );
 
 CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT, 
-  FOREIGN KEY(department_id REFERENCES department(id)
-  )
+  department_id INT,
+  FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR (30) NOT NULL,
-  last_name VARCHAR (30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT,
-   FOREIGN KEY(role_id) REFERENCES role (id) ON DELETE SET NULL, 
+  FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
   manager_id INT,
-   FOREIGN KEY(manager_id) REFERENCES employee (id) ON DELETE SET NULL 
+  FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
